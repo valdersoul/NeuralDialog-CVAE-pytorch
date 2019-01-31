f4a656a1d013f0daa570d5041be474cfcfc8816a
valder <valder.zju@gmail.com>
2019-01-31 16:38:01 +0800

    fix bug

--- models/cvae.py
+++ models/cvae.py
@@ -527,8 +527,10 @@ class KgRnnCVAE(BaseTFModel):
         if global_t is not None:
             feed_dict["global_t"] = global_t
 
-        #feed_dict = {k: torch.from_numpy(v).cuda() if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
-        feed_dict = {k: torch.from_numpy(v) if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
+        feed_dict = {k: torch.from_numpy(v).cuda() if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
+        #feed_dict = {k: torch.from_numpy(v) if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
+
+        return feed_dict
 
     def train_model(self, global_t, train_feed, update_limit=5000, use_profile=False):
         elbo_losses = []
@@ -921,8 +923,8 @@ class S2S(BaseTFModel):
         if global_t is not None:
             feed_dict["global_t"] = global_t
 
-        #feed_dict = {k: torch.from_numpy(v).cuda() if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
-        feed_dict = {k: torch.from_numpy(v) if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
+        feed_dict = {k: torch.from_numpy(v).cuda() if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
+        #feed_dict = {k: torch.from_numpy(v) if isinstance(v, np.ndarray) else v for k, v in feed_dict.items()}
 
         return feed_dict
 
