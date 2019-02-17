516f7185cc3965c51c0bcdaca1261d441f306026
valder <valder.zju@gmail.com>
2019-02-17 17:14:54 +0800

    fix bug

--- models/DirVAE.py
+++ models/DirVAE.py
@@ -306,10 +306,9 @@ class DirVAE(BaseTFModel):
                 
                 self.kl_w = kl_weights
                 self.elbo = self.avg_rc_loss + kl_weights * self.avg_kld
-                self.aug_elbo = self.avg_bow_loss + self.avg_da_loss + self.elbo
+                self.aug_elbo = self.avg_bow_loss + self.elbo
 
                 self.summary_op = [\
-                    tb.summary.scalar("model/loss/da_loss", self.avg_da_loss.item()),
                     tb.summary.scalar("model/loss/rc_loss", self.avg_rc_loss.item()),
                     tb.summary.scalar("model/loss/elbo", self.elbo.item()),
                     tb.summary.scalar("model/loss/kld", self.avg_kld.item()),
