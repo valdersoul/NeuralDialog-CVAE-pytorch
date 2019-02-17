ad5e97b3a1f4784c0a64ebcba2af9a7b91d4ed10
valder <valder.zju@gmail.com>
2019-02-17 17:29:28 +0800

    fix bug

--- models/DirVAE.py
+++ models/DirVAE.py
@@ -110,7 +110,7 @@ class DirVAE(BaseTFModel):
 
         # BOW loss
         self.bow_project = nn.Sequential(
-            nn.Linear(self.h_dim, 400),
+            nn.Linear(self.dec_cell_size, 400),
             nn.Tanh(),
             nn.Dropout(1 - config.keep_prob),
             nn.Linear(400, self.vocab_size)
@@ -244,7 +244,7 @@ class DirVAE(BaseTFModel):
             dec_init = self.dec_init_state_net(self.p).unsqueeze(0) 
 
             # BOW loss
-            self.bow_logits = self.bow_project(self.p)
+            self.bow_logits = self.bow_project(dec_init)
         
         with variable_scope.variable_scope("recogDecoder"):
             if mode == 'test':
