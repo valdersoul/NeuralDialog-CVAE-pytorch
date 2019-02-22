eb06e5f636f94abd177dd3a7b0e4028e464c801b
valder <valder.zju@gmail.com>
2019-02-22 20:56:32 +0800

    fuck

--- models/topicVAE.py
+++ models/topicVAE.py
@@ -400,7 +400,7 @@ class TopicVAE(BaseTFModel):
                     kl_weights = 1.0
 
                 self.kl_w = kl_weights
-                self.elbo = self.avg_rc_loss + kl_weights * (self.avg_kld + self.avg_kld_recog)
+                self.elbo = self.avg_rc_loss + #kl_weights * (self.avg_kld + self.avg_kld_recog)
                 self.elbo_recog = self.avg_rc_loss_recog + kl_weights * (self.avg_kld_recog)
                 self.aug_elbo = self.avg_bow_loss + self.avg_da_loss + self.elbo# + self.elbo_recog
 
